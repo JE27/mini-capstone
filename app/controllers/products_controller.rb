c61e40670c61c3a9da3166d062a432edf3f17dea
je27 <jocmoya@gmail.com>
2018-01-16 20:49:33 -0600

    changed to plural products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def one_product_method
+    products = Product.last
+    render json: products.as_json
+  end
+
+  def all_products_method
+    products = Product.all
+    render json: products.as_json
+  end
 end
