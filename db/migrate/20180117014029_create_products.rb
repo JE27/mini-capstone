75542af11b5b28193acbe024920299f3a93d639e
je27 <jocmoya@gmail.com>
2018-01-16 19:41:36 -0600

    recreated app

---
+++ db/migrate/20180117014029_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :price
+      t.string :ingredients
+      t.string :heat-level
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
