483117e3eef6fdd65d83145bca933b3fd767d34d
je27 <jocmoya@gmail.com>
2018-01-22 20:23:49 -0600

    migration:

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,30 @@
 class Product < ApplicationRecord
+
+  def discounted?
+    price < "2"
+  end 
+
+  def tax
+    price.to_i * 0.09
+  end
+
+  def total
+    tax + price.to_i
+  end
+
+ def as_json
+   {
+     name: name,
+     price: price,
+     tax: tax,
+     total: total,
+     is_discounted: discounted?,
+     ingredients: ingredients,
+     heat_level: heat_level,
+     description: description,
+     image_url: image_url
+   }
+
+ end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117014029) do
+ActiveRecord::Schema.define(version: 20180123022246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.decimal "price", precision: 6, scale: 2
     t.string "ingredients"
     t.string "heat_level"
-    t.string "description"
+    t.text "description"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "in_stock", default: true
   end
 
 end
